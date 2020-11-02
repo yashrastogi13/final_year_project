190e6fe123c465ce4b757ad3562bc1230dd8a66c
yashrastogi <yashrastogi.15082@gmail.com>
2020-11-02 17:45:51 +0530

    runned collectstatic

---
+++ static_cdn/static_root/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('#modal-btn').click(function(){
+        $('.ui.modal')
+        .modal('show');
+    })
+
+    $('.ui.dropdown').dropdown()
+
+    $('#modal-btn2').click(function(){
+        $('.ui.modal')
+        .modal('show');
+    })
+})
